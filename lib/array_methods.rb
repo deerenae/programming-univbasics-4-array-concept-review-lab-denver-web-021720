909d1658f7758ed22840756439b5ddfe4a54555c
Denise Magner <deerenae@gmail.com>
2020-02-06 00:32:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length do 
+  array.index(value_to_find)
+  counter += 1
+  end
+  counter
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min 
 end
